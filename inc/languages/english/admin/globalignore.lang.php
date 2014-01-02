066c15798a935e46b72c1445f8f40a5474f99f30
Kenny <info@mybbservice.de>
2014-01-02 21:38:46 +0100

    Englische Übersetzung
    Englische Übersetzung hinzugefügt

---
+++ inc/languages/english/admin/globalignore.lang.php
@@ -0,0 +1,30 @@
+<?php
+$l['globalignore'] = "Global Ignore";
+$l['globalignore_permissions'] = "Can manage Global Ignore?";
+$l['globalignore_no_id'] = "No ID submitted";
+$l['globalignore_wrong_id'] = "Wrong ID submitted";
+
+$l['list'] = "List";
+$l['list_desc'] = "All global ignored users";
+$l['templates'] = "Error messages";
+$l['templates_desc'] = "Manage your own error messages";
+
+$l['username'] = "User";
+$l['liftban'] = "Remove";
+$l['no_ignored'] = "No user to ignore";
+$l['ignored'] = "Global ignored user";
+
+$l['user_lifted'] = "Remove user from global ignore list";
+$l['user_added'] = "Set user on global ignore list";
+
+$l['add'] = "Add";
+
+$l['template'] = "Error message";
+$l['title'] = "Title";
+$l['no_templates'] = "No error messages added so far";
+$l['no_title'] = "No title specified";
+$l['no_template'] = "No error message specified";
+$l['template_added'] = "Error message saved";
+$l['template_deleted'] = "Error message deleted";
+$l['template_delete_confirm'] = "Are aou sure you want to delete this error mesaage?";
+?>
